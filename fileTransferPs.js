adf8cb067f830ed19e4d0af28b46ce42b2e85545
B-4a <bruk33@proton.me>
2024-12-10 16:50:16 -0500

    Update and rename fileTransferPs.xml to fileTransferPs.js

---
+++ fileTransferPs.js
@@ -0,0 +1,133 @@
+
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(Input.value.length <= 7) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+const URL = ("src:config.properties");
+
+   fetch("src:config.properties");
+
+      URL.then(function(response) {
+         console.log(response);
+        return response.json();
+   });
+
+      URL.then(function(data){
+  
+         console.log(data);
+      });
+const 
+img1 = document.queryselector('./submit');
+const
+button = document.queryselector('./length');
+
+
+document.addEventListner('click', function length(){
+if(i<=7, i<button.length); 
+(i='length');
+
+
+img1==JSON.stringify(['value.{i}'])
+
+console.log('$button.{value}.length$');
+}
+  function() {
+
+img1.({.container}).length.togle.classList.add("valid"));
+}
+console.log(img1);
+
+
+public class Encryption { 
+
+	// Main driver method 
+	//public static void main(String[client.js] args) 
+		//throws IOException, GeneralSecurityException
+	 
+		Properties properties = new Properties('client.js'); 
+		FileInputStream fileInputStream 
+			= new FileInputStream("src:config.properties"); 
+		properties.load(fileInputStream); 
+		String password 
+			= properties.getProperty("Password"); 
+
+		if (password == null) { 
+			throw new IllegalArgumentException( 
+				"Parameter is not present in configuration file"); 
+		} 
+
+		byte[] 'client.js' = new String("client.js").getBytes(); 
+		int iterationCount = 7; 
+		int keyLength = 7; 
+		
+		secretKey object = new secretKey('client.js); 
+		SecretKeySpec key = object.generateSecretKey( 
+			password.toCharArray('client.js'), salt, iterationCount, 
+			keyLength)); 
+
+		'client.js'= password; 
+		System.out.println('client.js: 
+						+ originalPassword'); 
+		String encryptedPassword 
+			= object.encrypt(originalPassword, key); 
+		
+		System.out.println("Encrypted password: "
+						+ encryptedPassword); 
+	} 
+	console.log(String.getValue([client.js]));
+} 
+  
+
+
