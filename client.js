2fba5e09a903f84f748baa029489ed3f76f1236d
B-4a <bruk33@proton.me>
2024-12-10 16:44:57 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,10 @@
+document.addQuerySelector();
+const pixels = new int[4*x*y];
 
+pixels[0] = xRed (Iterations ++ function (Red){1.0f,0.0f,0.2f};
+
+float[0] = 1.0f{yRed};
+short[0] =0.0f {doubleRed});
+pixels.setPixels('red');
+ 
+console log('${Red}$');
