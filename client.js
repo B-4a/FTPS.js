c3b9c6d083210f5ba3c80f988e01daf15e9ab803
B-4a <bruk33@proton.me>
2024-12-11 15:24:46 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
-document.addQuerySelector('./img001.jpg');
+pixels = document.addQuerySelector('./img001.jpg');
+
 const pixels = new int[4*x*y];
 
 pixels[0] = xRed (Iterations ++ function (Red){1.0f,0.0f,0.2f};
