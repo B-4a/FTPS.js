94795a72d60178c9b7bc32ea34da822b50f585e6
#B-290 <bruk33@proton.me>
2025-07-28 21:11:37 -0400

    Update client.js

--- client.js
+++ client.js
@@ -9,3 +9,15 @@ short[0] =0.0f {doubleRed});
 pixels.setPixels('red');
  
 console log('${Red}$');
+pixels = document.addQuerySelector('.p:Input');
+
+const pixels = new int[4*x*y];
+
+pixels[0] = xRed (Iterations ++ function (Red){1.0f,0.0f,0.2f};
+
+float[0] = 1.0f{yRed};
+short[0] =0.0f {doubleRed});
+pixels.setPixels('red');
+ 
+console log('${Red}$');
+
