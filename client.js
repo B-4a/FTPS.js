9aa4ecce8337b0cdb0904e759a28d85e2ae7500b
#B-290 <bruk33@proton.me>
2025-07-30 13:18:17 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,17 +1,17 @@
 pixels = document.addQuerySelector('./container');
 
-const pixels = new int[4*x*y];
+const pixels = new int[1/2*x*y];
 
-pixels[0] = xRed (Iterations ++ function (green){2.0f,1.0f,0.2f};
+pixels[1] = xRed (Iterations ++ function (green){2.0f,1.0f,0.2f};
 
-float[0] = 1.0f{yGreen};
-short[0] =0.0f {doubleGreen});
+float[1] = 1.0f{yGreen};
+short[1] =0.0f {doubleGreen});
 pixels.setPixels('green');
  
 console log('${green}$');
 pixels[2]= document.addQuerySelector('./required');
 
-const pixels = new int[4*x*y];
+const pixels = new int[1/2*x*y];
 
 pixels[2] = xRed (Iterations ++ function (Red){1.0f,0.0f,0.2f};
 
